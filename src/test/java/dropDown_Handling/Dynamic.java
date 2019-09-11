73d832ff28d28099b8a67a6c9d2c11c4ea319d03
Arpit <Arpit@Tyss-pt-011>
2019-09-11 12:29:17 +0530

    Updating_Dynamic

--- src/test/java/dropDown_Handling/Dynamic.java
+++ src/test/java/dropDown_Handling/Dynamic.java
@@ -3,6 +3,7 @@ package dropDown_Handling;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.text.WordUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,7 +14,8 @@ import utils.DriverCreation;
 
 public class Dynamic extends DriverCreation
 {
-	public static void main(String[] args)
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException
 	{
 		DriverCreation DC = new DriverCreation();
 		WebDriver driver = DC.driver("chrome");
@@ -23,7 +25,7 @@ public class Dynamic extends DriverCreation
 		
 		driver.get("https://www.google.co.in/");
 		
-		String search = "Java";
+		String search = "seLenium";
 		
 		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys(search);
 		
@@ -32,11 +34,32 @@ public class Dynamic extends DriverCreation
 		for(int i = 2 ; i <= Sugestions.size() ; i++ )
 		{
 			WebElement Value = driver.findElement(By.xpath("//ul[@role='listbox']/li["+i+"]/div[1]/div[2]/div[1]/span[1]/b"));
-			System.out.println(Value.getText());
+			System.out.println(search+Value.getText());
 		}
 		
-		driver.findElement(By.xpath("//input[@title='Search']")).clear();
+		driver.findElement(By.xpath("//ul[@role='listbox']/li["+2+"]/div[1]/div[2]/div[1]/span[1]/b")).click();
 		
+		ArrayList<WebElement> Searches = (ArrayList<WebElement>)driver.findElements(By.xpath("//h1[text()='Search Results']/..//a/div"));
+		
+		W.until(ExpectedConditions.elementToBeClickable(Searches.get(1)));
+		Searches.get(1).click();
+		
+		try 
+		{
+			String Header = driver.findElement(By.xpath("//h1[1]")).getText();
+			if(Header.contains(search.toLowerCase())||Header.contains(WordUtils.capitalizeFully(search,' '))||Header.contains(search.toUpperCase()))
+				System.out.println("\nPage Header '"+Header+"' contains the search '"+search+"'");
+			else
+				System.out.println("\nPage Header '"+Header+"' dose not contains the search "+search+"'");
+
+		}
+		catch(Throwable E)
+		{
+			System.out.println("Page Header not found by the locater used.");
+		}
+		
+		Thread.sleep(1000);
+
 		driver.close();
 	}
 }
