7a2eae0c0d6cb50cc952946ed9d03d135a718757
Admin <Admin@Admin-PC>
2019-09-12 00:59:37 +0530

    Updating_DropDownMenu

--- src/test/java/dropDown_Handling/DropDownMenu.java
+++ src/test/java/dropDown_Handling/DropDownMenu.java
@@ -1,11 +1,14 @@
 package dropDown_Handling;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.DriverCreation;
 
@@ -15,12 +18,20 @@ public class DropDownMenu extends DriverCreation
 	DriverCreation DC = new DriverCreation();
 	static WebDriver driver;
 	
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		DriverCreation DC = new DriverCreation();
 		driver = DC.driver("chrome");
 		
 		driver.get( "https://www.urbanladder.com/" );
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+		WebDriverWait W = new WebDriverWait(driver, 15);
+		
+		WebElement Close = driver.findElement(By.xpath("//a[contains(text(),'Close')]"));
+		W.until(ExpectedConditions.elementToBeClickable(Close));
+		Close.click();
 		
 		// Common Xpath for all dropdown menus
 		String DropdownMenus = "//span[@class='topnav_itemname']";
@@ -28,32 +39,37 @@ public class DropDownMenu extends DriverCreation
 		// Identifying all DropDown Menus WebElements
 		List<WebElement> Menus = driver.findElements(By.xpath(DropdownMenus));
 		
-		for( int i = 0 ; i < Menus.size() ; i++ )
+		for( int i = 1 ; i <= Menus.size() ; i++ )
 		{	
+			Actions A = new Actions(driver);
+			A.moveToElement(Menus.get(i-1)).perform();
+			
 			// Reading Header text of each dropdown menus one-by-one
-			String HeaderText = Menus.get(i).getText();
+			String HeaderText = Menus.get(i-1).getText();
 			
 			// Printing Header text from each dropdown menus one-by-one
-			System.out.println( "Menu[" + (i+1) + "] = " + HeaderText + "\n" );
+			System.out.println( "Menu[" + (i) + "] = " + HeaderText + "\n" );
 
-			// Collecting all sub list Headers and Items as WebElements -> present inside each dropdown menu
+			// Collecting all sub list Headers as WebElements -> present inside each dropdown menu
 			List<WebElement> SubLists = (List<WebElement>)driver.findElements(By.xpath( "//span[contains(text(),'" + HeaderText + "')]/../div//div/ul/li["+i+"]/div/a" ));
 			
 			for( int j = 1 ; j < SubLists.size() ; j++ )
 			{	
+				W.until(ExpectedConditions.elementToBeClickable(SubLists.get(j-1)));
+				// Printing all sub list Headers -> present inside each dropdown menus as Coloumn Headings
+				System.out.print( "Sub List[" + (j) + "] = " + SubLists.get(j-1).getText() + " -> List Items = [" );
+
 				// Collecting all items as WebElements -> present inside each sub list -> present inside each dropdown menu as Coloumn
-				List<WebElement> subListItems = (List<WebElement>)driver.findElements(By.xpath( "//span[contains(text(),'" + HeaderText + "')]/../../li[1]/div/div/ul/li["+j+"]/ul/li/a/span" ));
+				List<WebElement> subListItems = (List<WebElement>)driver.findElements(By.xpath( "//span[contains(text(),'" + HeaderText + "')]/../div/div/ul/li["+j+"]/ul/li/a/span" ));
 				
-				for( int k = 1 ; k < subListItems.size()-1 ; k++ )
+				for( int k = 0 ; k < subListItems.size()-1 ; k++ )
 				{
-					// Printing all sub list Headers -> present inside each dropdown menus as Coloumn Headings
-					System.out.print( "Sub List[" + (i+1) + "] = " + subListItems.get(0) + " -> List Items = [" );
-					
-					for( int l = 1 ; l < subListItems.size()-1 ; l++ )
-					// Printing all items -> present inside each sub lists -> present inside each dropdown menus as Coloumn values one-by-one
-					System.out.print( subListItems.get(l) + "," );
+					W.until(ExpectedConditions.elementToBeClickable(subListItems.get(k-1)));
+					// Printing all items -> present inside each sub list -> present inside each dropdown menu as Coloumn values one-by-one
+					System.out.print( subListItems.get(k).getText() + "," );
 				}
-				System.out.println( subListItems.get(subListItems.size()-1) + "]\n" );
+				W.until(ExpectedConditions.elementToBeClickable(subListItems.get(subListItems.size()-1)));
+				System.out.println( subListItems.get(subListItems.size()-1).getText() + "]\n" );
 			}
 		}
 		
